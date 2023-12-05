1e84930319380455e03218d48fb491181a009b01
Hermans Cedric <cedric.hermans2@howest.be>
2023-12-05 16:06:11 +0100

    Challenge 1 for day 5 solved, 2 will be difficult 😬

---
+++ day5/challenge1.py
@@ -0,0 +1,20 @@
+with open("challengeinput.txt","r") as ifile:
+#with open("testinput.txt","r") as ifile:
+    lines=ifile.read()
+
+sections=lines.split("\n\n")
+seeds=[int(x) for x in sections[0].split(" ")[1:]]
+# note to self: dest,src,len
+steps=[[[int(y) for y in x.split(" ")]  for x in sections[n].split("\n")[1:]] for n in range(1,len(sections))]
+#for step in steps:
+#    print(step)
+finalseeds=[]
+for seed in seeds:
+    for step in steps:
+        for (dest,src,len) in step:
+            if src<=seed<src+len:
+                #print("seed",seed,"dest",dest,"src",src,"len",len, "diff",dest-src)
+                seed+=(dest-src)
+                break
+    finalseeds.append(seed)
+print(min(finalseeds))
